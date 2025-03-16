a05b27a3b725b4e3d691a6797f926434d92f3e7a
Muhammad omer <sayyidmuhammadomer@gmail.com>
2025-03-16 19:44:15 +0500

    Delete src/app/user/trainer-review directory

--- src/app/user/trainer-review/[trainereid]/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const SingleTrainerReviewPage = () => {
-  return (
-    <div>SingleTrainerReviewPage</div>
-  )
-}
-
-export default SingleTrainerReviewPage
--- src/app/user/trainer-review/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const TrainerReviewPage = () => {
-  return (
-    <div>TrainerReviewPage</div>
-  )
-}
-
-export default TrainerReviewPage
