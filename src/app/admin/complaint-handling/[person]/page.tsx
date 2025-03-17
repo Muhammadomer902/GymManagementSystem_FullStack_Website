18259541faf4c6643d19fe878cc6df22df1e38f9
Muhammad omer <sayyidmuhammadomer@gmail.com>
2025-03-17 21:18:42 +0500

    Delete src/app/admin/complaint-handling/[person] directory

--- src/app/admin/complaint-handling/[person]/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const ComplaintHandlingPersonPage = () => {
-  return (
-    <div>ComplaintHandlingPersonPage</div>
-  )
-}
-
-export default ComplaintHandlingPersonPage
